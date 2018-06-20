db418a168de0975013cbe99ca31ce82fff6134f9
asthaparashar05 <asthaparashar05@gmail.com>
2018-06-20 18:45:05 +0530

    dsg

--- assignment.queryreport.rest/src/test/java/nl/lunatech/assignment/queryreport/rest/AppTest.java
+++ assignment.queryreport.rest/src/test/java/nl/lunatech/assignment/queryreport/rest/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple Application class.
  */
 public class AppTest 
     extends TestCase

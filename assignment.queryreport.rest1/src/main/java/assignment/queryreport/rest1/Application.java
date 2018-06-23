b8464b1cb71a2f89b29f673ff942d8abf852ee48
asthaparashar05 <40427668+asthaparashar05@users.noreply.github.com>
2018-06-23 23:09:22 +0530

    Checking in

---
+++ assignment.queryreport.rest1/src/main/java/assignment/queryreport/rest1/Application.java
@@ -0,0 +1,22 @@
+package assignment.queryreport.rest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Main Application class 
+ *
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages="assignment.queryreport.rest1")
+
+public class Application 
+{
+	 public static void main(String[] args) throws Exception {
+	        SpringApplication.run(Application.class, args);
+		 
+	    }
+}

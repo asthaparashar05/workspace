1c4e9e579b34db0a7bcea233e86d39d4fe351727
asthaparashar05 <asthaparashar05@gmail.com>
2018-07-09 20:48:33 +0530

    Adding comments

--- swapi.js.wrapper/WebContent/lib/swapi-starwars.js
+++ swapi.js.wrapper/WebContent/lib/swapi-starwars.js
@@ -46,6 +46,7 @@ var module = function () {
 //To get root resource (with details of other resources)
 function getAllResources() {
 	module.getAllResources(function(data) {
+		//Logging the response data on console and assigning it to a DOM element. Below code in the function can be changed as per the requirement.
 		console.log("getAllResources data", data);
 		var stringData=JSON.stringify(data, undefined, 4);
 		document.getElementById("allResources").innerHTML = stringData;
@@ -55,6 +56,7 @@ function getAllResources() {
 //To get all the data for a resource
 function getAllForResource(resourcePath) {
 	module.getAllForResource(resourcePath,function(data) {
+		//Logging the response data on console and assigning it to a DOM element. Below code in the function can be changed as per the requirement.
 		console.log("getAllForResource data", data);
 		var stringData=JSON.stringify(data, undefined, 4);
 		document.getElementById("resource").innerHTML = stringData;
@@ -64,6 +66,7 @@ function getAllForResource(resourcePath) {
 //To a single record for people resource by id
 function getPeopleById(id) {
 	module.getResourceById('people',id,function(data) {
+		//Logging the response data on console and assigning it to a DOM element. Below code in the function can be changed as per the requirement.
 		console.log("getPeopleById data", data);
 		var stringData=JSON.stringify(data, undefined, 4);
 		document.getElementById("people").innerHTML = stringData;
@@ -73,6 +76,7 @@ function getPeopleById(id) {
 //To a single record for films resource by id
 function getFilmById(id) {
 	module.getResourceById('films',id,function(data) {
+		//Logging the response data on console and assigning it to a DOM element. Below code in the function can be changed as per the requirement.
 		console.log("getFilmById data", data);
 		var stringData=JSON.stringify(data, undefined, 4);
 		document.getElementById("films").innerHTML = stringData;
@@ -82,6 +86,7 @@ function getFilmById(id) {
 //To a single record for starships resource by id
 function getStarshipById(id) {
 	module.getResourceById('starships',id,function(data) {
+		//Logging the response data on console and assigning it to a DOM element. Below code in the function can be changed as per the requirement.
 		console.log("getStarshipById data", data);
 		var stringData=JSON.stringify(data, undefined, 4);
 		document.getElementById("starships").innerHTML = stringData;
@@ -91,6 +96,7 @@ function getStarshipById(id) {
 //To a single record for vehicles resource by id
 function getVehicleById(id) {
 	module.getResourceById('vehicles',id,function(data) {
+		//Logging the response data on console and assigning it to a DOM element. Below code in the function can be changed as per the requirement.
 		console.log("getVehicleById data", data);
 		var stringData=JSON.stringify(data, undefined, 4);
 		document.getElementById("vehicles").innerHTML = stringData;
@@ -100,6 +106,7 @@ function getVehicleById(id) {
 //To a single record for species resource by id
 function getSpeciesById(id) {
 	module.getResourceById('species',id,function(data) {
+		//Logging the response data on console and assigning it to a DOM element. Below code in the function can be changed as per the requirement.
 		console.log("getSpeciesById data", data);
 		var stringData=JSON.stringify(data, undefined, 4);
 		document.getElementById("species").innerHTML = stringData;
@@ -109,6 +116,7 @@ function getSpeciesById(id) {
 //To a single record for planets resource by id
 function getPlanetById(id) {
 	module.getResourceById('planets',id,function(data) {
+		//Logging the response data on console and assigning it to a DOM element. Below code in the function can be changed as per the requirement.
 		console.log("getPlanetById data", data);
 		var stringData=JSON.stringify(data, undefined, 4);
 		document.getElementById("planets").innerHTML = stringData;
